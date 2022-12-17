963e5809a6b55f022dddad82f6954f4ce69b8c14
ziku10121 <ziku10121@gmail.com>
2022-12-18 04:19:56 +0800

    update dist

--- dist/js/about.feab6ac5.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkvuecli"]=self["webpackChunkvuecli"]||[]).push([[443],{7381:function(n,u,e){e.r(u),e.d(u,{default:function(){return f}});var t=e(3396);const a={class:"about"},c=(0,t._)("h1",null,"This is an about page",-1),r=[c];function s(n,u){return(0,t.wg)(),(0,t.iD)("div",a,r)}var i=e(89);const l={},o=(0,i.Z)(l,[["render",s]]);var f=o}}]);
-//# sourceMappingURL=about.feab6ac5.js.map
